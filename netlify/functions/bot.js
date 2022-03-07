a7e3aeb194ad8630d4dd43c6a058d752389a18d9
pnts <panagiotis.tsivelekidis@gmail.com>
2022-03-07 08:08:38 +0100

    testing

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
   // bot.handleUpdate(tmp)
   return {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: JSON.stringify(context),
   }
 }
 
