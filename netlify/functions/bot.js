85e634cd268226dd3c84b3070a170ea4fde7434e
pnts <panagiotis.tsivelekidis@gmail.com>
2022-03-07 09:18:19 +0100

    testing

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -17,6 +17,8 @@ exports.handler = async (event, context) => {
   // console.log(event.body);
   let body = event.body
   console.log(body);
+  const bodyObject = JSON.parse(body)
+  console.log(bodyObject);
   return {
     statusCode: 200,
     body: "OK",
